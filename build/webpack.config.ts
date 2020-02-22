1309448e30a2fabff5d6a5daacd2c7f7f78cb3cd
Anders Liu <anders.liu@gmail.com>
2020-02-22 10:18:07 -0800

    Build for tsx and react.

--- build/webpack.config.ts
+++ build/webpack.config.ts
@@ -5,24 +5,45 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 const config: ConfigurationFactory = (_env, args) => {
     const isProd = args.mode === "production";
 
+    const buildDir = resolve(__dirname);
     const srcDir = resolve(__dirname, "../src");
     const distDir = resolve(__dirname,
         "../dist", isProd ? "prod" : "dev");
 
     const appTitle = isProd ? "Bing At Work" : "Bing At Work (DEV)";
+    const tsConfigFile = isProd
+        ? resolve(buildDir, "tsconfig.prod.json")
+        : resolve(buildDir, "tsconfig.dev.json");
 
     return {
         entry: {
-            popup: resolve(srcDir, "browser-action/index.ts"),
-            content: resolve(srcDir, "content-script/index.ts")
+            popup: resolve(srcDir, "browser-action/index.tsx"),
+            content: resolve(srcDir, "content-script/index.tsx")
         },
         output: {
             filename: "[name].js",
             path: distDir
         },
+        module: {
+            rules: [{
+                test: /\.tsx?$/,
+                use: [{
+                    loader: "ts-loader",
+                    options: {
+                        configFile: tsConfigFile
+                    }
+                }],
+                exclude: /node_modules/
+            }]
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"]
+        },
+        devtool: isProd ? undefined : "inline-source-map",
         plugins: [
             new HtmlWebpackPlugin({
                 filename: "popup.html",
+                template: resolve(srcDir, "browser-action/index.html"),
                 title: appTitle,
                 chunks: ["popup"],
                 minify: isProd ? {
---
+++ src/browser-action/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppRoot } from "./components/app-root";
+
+ReactDOM.render(
+    <AppRoot />,
+    document.getElementById("app-root"));
