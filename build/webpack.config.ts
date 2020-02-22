5f0ddc1b496c28a59343a14e8b1b469d5aa69e87
Anders Liu <anders.liu@gmail.com>
2020-02-22 00:50:59 -0800

    Add basic build files.

---
+++ build/webpack.config.ts
@@ -0,0 +1,10 @@
+import * as path from "path";
+import * as webpack from "webpack";
+
+const config: webpack.ConfigurationFactory = (env, args) => {
+    return {
+
+    };
+};
+
+export default config;
